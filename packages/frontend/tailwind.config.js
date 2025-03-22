990d8b9401461f4523f6a0bc350aa95f7ebb18b4
adriangohjw <adriangohjw@gmail.com>
2025-03-22 09:21:37 +0800

    add tailwind

---
+++ packages/frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'class' for manual dark mode toggling
+  theme: {},
+  plugins: [],
+};
