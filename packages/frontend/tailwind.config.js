c0b10f5b4e5b5ad2021219c7fb90604a7513c6c8
adriangohjw <adriangohjw@gmail.com>
2025-03-22 15:47:38 +0800

    Update package-lock.json and package.json to add new optional Tailwind CSS dependencies and modify build script to use cross-env for environment variable management. Enhance Tailwind configuration to extend theme settings.

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,6 +2,8 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // or 'class' for manual dark mode toggling
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
